b9afcce5c49080ec6d6622e15df37d1133941fda
tonyh <star4evar@gmail.com>
2022-09-03 17:15:12 +0800

    reverse proxy server finished and documented

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # http_reverse_proxy
 a practice project , implement http reverse proxy with rust, following https://www.youtube.com/watch?v=FcHYQMRfGWw&amp;list=WL&amp;index=1
+
+
+following links may be helpful understanding the code:
+
+https://www.fpcomplete.com/blog/captures-closures-async/
+
+https://users.rust-lang.org/t/closure-may-outlive-the-current-function/79282
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+extern crate hyper;
+
+use hyper::{Client, Server, Request, Response, Body};
+use anyhow::*;
+use std::net::SocketAddr;
+use hyper::service::{make_service_fn, service_fn};
+use std::sync::{Arc, RwLock};
+
+const TARGET_HOST:&str = "https://www.snoyman.com";
+
+fn mutate_request(req: &mut Request<Body>) -> Result<()>{
+    for key in &["content-length","transfer-encoding", "accept-encoding", "content-encoding" ]{
+        req.headers_mut().remove(*key);
+    }
+
+    let uri = req.uri();
+    let uri_string = match uri.query(){
+        None => format!("{}{}", TARGET_HOST,  uri.path()),
+        Some(query) => format!("{}{}?{}", TARGET_HOST, uri.path(), query),
+    };
+    *req.uri_mut() = uri_string.parse().context("Parsing URI in mutate request")?;
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Stats{
+    proxied: usize,
+}
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    let https = hyper_rustls::HttpsConnector::with_native_roots();
+    let client: Client<_, hyper::Body> = Client::builder().build(https);
+    let client = Arc::new(client);
+    let stats: Arc<RwLock<Stats>> = Arc::new(RwLock::new(Stats{
+        proxied: 0,
+    }));
+
+    // the outer lambda does not need a 'move' keyword because variable 'make_svc' will not outlive 'client'
+    let make_svc = make_service_fn(|_| {
+        // rust does not allow moving of lambda-captured variables, so we need to clone them
+        // and thereby having local variables for moving( by the async block ).
+        let client = client.clone();
+        let stats = stats.clone();
+        // async blocks are essentially lambdas run in an other thread, which will outlive it's wrapping function
+        //  (and variables defined in it), so variables it uses must be moved into it rather than referred
+        async move {
+            let client = client.clone();
+            let stats = stats.clone();
+            // the inner lambda NEED a 'move' keyword because this lambda is wrapped into a Future<Result<...>>
+            // and returned out of it's defining async block where the 'client' and 'stats' are also defined.
+            // so this lambda will outlive variable 'client' and 'stats'.
+            Ok(service_fn(move |mut req| {
+                let client = client.clone();
+                let stats = stats.clone();
+                // inner async block also need a move keyword, for the same reason that the outer async block needs one.
+                async move {
+                    if req.uri().path() == "/status" {
+                        let body: Body = format!("{:?}", stats.read().unwrap()).into();
+                        Ok(Response::new(body))
+                    } else {
+                        println!("Proxied: {}", req.uri().path());
+                        stats.write().unwrap().proxied += 1;
+                        mutate_request(&mut req)?;
+                        client.request(req).await.context("Making request to backend server.")
+                    }
+                }
+            }))
+        }
+    });
+
+    let addr = SocketAddr::from(([0,0,0,0], 3000));
+    Server::bind(&addr).serve(make_svc).await.context("Running server")?;
+
+    Ok(())
 }
